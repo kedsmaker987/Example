e9c750b11db56a3ed573eaba39aece3b1140facc
kedsmaker987 <kedsmake@163.com>
2017-12-09 16:51:03 +0800

    config

--- Hello.js
+++ Hello.js
@@ -1,10 +1,11 @@
 function hello(){
 	var obj = document.getElementById(id);
 	obj.onmousedown = function(){
-		var disX = 0;
+		var dizZ = 12;
 		var disY = 0;
 		var disZ = 0;
 		var disXY = 0;
+		var disYZ = 0;
 	}
 		
 }
